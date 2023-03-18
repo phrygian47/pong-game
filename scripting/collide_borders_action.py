cc912bb4efa607f793c92c8b11ab2223445b322f
phrygian47 <Robert.elliott2925@gmail.com>
2023-03-18 16:59:42 -0600

    comments

--- scripting/collide_borders_action.py
+++ scripting/collide_borders_action.py
@@ -24,13 +24,17 @@ class CollideBordersAction(Action):
         score_sound = Sound(SCORE_SOUND)
                 
         if x < (FIELD_LEFT):
+            # If the ball hits the left side of the screen, update points and reset ball position.
             self._audio_service.play_sound(score_sound)
             stats_l = cast.get_first_actor(STATS_L_GROUP)
             stats_l.add_points(1)
-            
-            if stats_l.get_score() == 10:
+            # Conditional to check if the player has won.
+            if stats_l.get_score() == 2:
+                # Change scene to game over screen
                 callback.on_next(GAME_OVER)
+                # Play game over sound
                 self._audio_service.play_sound(over_sound) 
+            # Clear the ball and reset its position.
             cast.clear_actors(BALL_GROUP)
             x = CENTER_X - BALL_WIDTH / 2
             y = CENTER_Y
@@ -41,17 +45,22 @@ class CollideBordersAction(Action):
             image = Image(BALL_IMAGE)
             ball = Ball(body, image, True)
             cast.add_actor(BALL_GROUP, ball)
+            # Delay to allow time to prepare
             time.sleep(1)
             ball.release()
+        
+        # Game logic for if the ball hits the right side of the screen.
         if  x > (FIELD_RIGHT-BALL_WIDTH):
             self._audio_service.play_sound(score_sound)
             stats_r = cast.get_first_actor(STATS_R_GROUP)
             stats_r.add_points(1)
             
-            if stats_r.get_score() == 10:
+            #Conditional check if this player has won the game.
+            if stats_r.get_score() == 2:
                 callback.on_next(GAME_OVER)
                 self._audio_service.play_sound(over_sound) 
-        
+
+            # Clear ball and reset position.
             cast.clear_actors(BALL_GROUP)
             x = CENTER_X - BALL_WIDTH / 2
             y = CENTER_Y
@@ -64,6 +73,8 @@ class CollideBordersAction(Action):
             cast.add_actor(BALL_GROUP, ball)
             time.sleep(1)
             ball.release()
+            
+        # If the ball hits the top or bottom, invert y velocity.
         if y <= FIELD_TOP:
             ball.bounce_y()
             self._audio_service.play_sound(bounce_sound)
